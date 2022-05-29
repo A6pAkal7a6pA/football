f76d52396d9f8447171148d26c3aede0a95bc5ab
Maksym Kuleshov <maksym_kuleshov@epam.com>
2022-05-29 12:42:10 +0300

    changed color

--- js/script.js
+++ js/script.js
@@ -137,9 +137,106 @@ let headSlider = new Swiper('.head-slider', {
 	// }
 });
 
-let firstChildRoadItem = document.querySelector('.road-top .road__item:first-child');
-let marginMidLine = firstChildRoadItem.getBoundingClientRect().width / 2;
-document.querySelector('.road-mid').style.marginLeft = marginMidLine + 'px';
-document.querySelector('.road-bot').style.marginLeft = marginMidLine + 10 + 'px';
-let checkInner = document.querySelector('.check__inner');
-document.querySelector('.road__inner').style.marginLeft = checkInner.getBoundingClientRect().left + 'px';
+function setUpConfiguration() {
+	let firstChildRoadItem = document.querySelector('.road-top .road__item:first-child');
+	let marginMidLine = firstChildRoadItem.getBoundingClientRect().width / 2;
+	document.querySelector('.road-mid').style.marginLeft = marginMidLine + 'px';
+	document.querySelector('.road-bot').style.marginLeft = marginMidLine + 10 + 'px';
+	let checkInner = document.querySelector('.check__inner');
+	document.querySelector('.road__inner').style.marginLeft = checkInner.getBoundingClientRect().left + 'px';
+}
+
+setUpConfiguration();
+
+function getSiblings(elem) {
+	var siblings = [];
+	var sibling = elem;
+	while (sibling.previousSibling) {
+		sibling = sibling.previousSibling;
+		sibling.nodeType == 1 && siblings.push(sibling);
+	}
+
+	sibling = elem;
+	while (sibling.nextSibling) {
+		sibling = sibling.nextSibling;
+		sibling.nodeType == 1 && siblings.push(sibling);
+	}
+
+	return siblings;
+}
+
+
+let headSlides = document.querySelectorAll('.head-slider__slide');
+let colors = document.querySelectorAll('.custom__color');
+
+headSlides.forEach((slide) => {
+	slide.addEventListener('click', () => {
+		slide.classList.add('active');
+		getSiblings(slide).forEach((slide) => slide.classList.remove('active'));
+	});
+});
+
+colors.forEach((slide) => {
+	slide.addEventListener('click', () => {
+		slide.classList.add('active');
+		document.querySelector('.custom-human__body').src = skins[slide.classList[2]].body;
+		document.querySelector('.custom-human__right-shoulder').src = skins[slide.classList[2]].rightShoulder;
+		document.querySelector('.custom-human__left-shoulder').src = skins[slide.classList[2]].leftShoulder;
+		document.querySelector('.custom-human__right-hand').src = skins[slide.classList[2]].rightHand;
+		document.querySelector('.custom-human__left-hand').src = skins[slide.classList[2]].leftHand;
+		document.querySelector('.custom-human__legs').src = skins[slide.classList[2]].legs;
+
+		getSiblings(slide).forEach((slide) => slide.classList.remove('active'));
+	});
+});
+
+let skins = {
+	darkBlack: {
+		body: 'img/customization/colors/body_darkBlack.png',
+		leftShoulder: 'img/customization/colors/leftHand_darkBlack.png',
+		leftHand: 'img/customization/colors/leftFist_darkBlack.png',
+		rightShoulder: 'img/customization/colors/rightHand_darkBlack.png',
+		rightHand: 'img/customization/colors/rightFist_darkBlack.png',
+		legs: 'img/customization/colors/legs_darkBlack.png'
+	},
+	darkBrown: {
+		body: 'img/customization/colors/body_darkBrown.png',
+		leftShoulder: 'img/customization/colors/leftHand_darkBrown.png',
+		leftHand: 'img/customization/colors/leftFist_darkBrown.png',
+		rightShoulder: 'img/customization/colors/rightHand_darkBrown.png',
+		rightHand: 'img/customization/colors/rightFist_darkBrown.png',
+		legs: 'img/customization/colors/legs_darkBrown.png'
+	},
+	darkChocolate: {
+		body: 'img/customization/colors/body_darkChocolate.png',
+		leftShoulder: 'img/customization/colors/leftHand_darkChocolate.png',
+		leftHand: 'img/customization/colors/leftFist_darkChocolate.png',
+		rightShoulder: 'img/customization/colors/rightHand_darkChocolate.png',
+		rightHand: 'img/customization/colors/rightFist_darkChocolate.png',
+		legs: 'img/customization/colors/legs_darkChocolate.png'
+	},
+	lightBrick: {
+		body: 'img/customization/colors/body_lightBrick.png',
+		leftShoulder: 'img/customization/colors/leftHand_lightBrick.png',
+		leftHand: 'img/customization/colors/leftFist_lightBrick.png',
+		rightShoulder: 'img/customization/colors/rightHand_lightBrick.png',
+		rightHand: 'img/customization/colors/rightFist_lightBrick.png',
+		legs: 'img/customization/colors/legs_lightBrick.png'
+	},
+	lightChiz: {
+		body: 'img/customization/colors/body_lightChiz.png',
+		leftShoulder: 'img/customization/colors/leftHand_lightChiz.png',
+		leftHand: 'img/customization/colors/leftFist_lightChiz.png',
+		rightShoulder: 'img/customization/colors/rightHand_lightChiz.png',
+		rightHand: 'img/customization/colors/rightFist_lightChiz.png',
+		legs: 'img/customization/colors/legs_lightChiz.png'
+	},
+	lightOrange: {
+		body: 'img/customization/colors/body_lightOrange.png',
+		leftShoulder: 'img/customization/colors/leftHand_lightOrange.png',
+		leftHand: 'img/customization/colors/leftFist_lightOrange.png',
+		rightShoulder: 'img/customization/colors/rightHand_lightOrange.png',
+		rightHand: 'img/customization/colors/rightFist_lightOrange.png',
+		legs: 'img/customization/colors/legs_lightOrange.png'
+	},
+}
