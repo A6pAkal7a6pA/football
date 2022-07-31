3d7f3fa9cd8535571a7dd9220e21ba6d358f1046
Maksym Kuleshov <maksym_kuleshov@epam.com>
2022-07-31 22:03:21 +0300

    added animation lottie

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+let slideIndex = 0;
+carousel();
+
+function carousel() {
+	let x = document.getElementsByClassName("main__slideshow");
+	for (let i = 0; i < x.length; i++) {
+		x[i].style.display = "none";
+	}
+	slideIndex++;
+	if (slideIndex > x.length) { slideIndex = 1 }
+	x[slideIndex - 1].style.display = "block";
+	setTimeout(carousel, 3000); // Change image every 2 seconds
+}
+
 setTimeout(() => {
 	const storeSlider = new Swiper('.store-slider', {
 		loop: true,
@@ -576,16 +590,4 @@ ballPosition();
 let secretBall = document.querySelector('.secret-ball');
 secretBall.addEventListener('click', () => secretBall.classList.toggle('active'))
 
-let slideIndex = 0;
-carousel();
 
-function carousel() {
-	let x = document.getElementsByClassName("main__slideshow");
-	for (let i = 0; i < x.length; i++) {
-		x[i].style.display = "none";
-	}
-	slideIndex++;
-	if (slideIndex > x.length) { slideIndex = 1 }
-	x[slideIndex - 1].style.display = "block";
-	setTimeout(carousel, 3000); // Change image every 2 seconds
-}
